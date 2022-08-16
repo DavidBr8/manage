5fb6fa02c9e222a3c0b95a9c9ec8c8112d20fad4
David <brozdawid@gmail.com>
2022-08-16 16:43:20 +0200

    Nav update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const navBtn = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.nav__mobile');
+const shadowHandle = document.querySelector('.pageshadow');
+
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
-	navMobile.classList.toggle('nav-mobile--active');
+	mobileNav.classList.toggle('--active');
+	shadowHandle.classList.toggle('pageshadow-active');
 };
 navBtn.addEventListener('click', handleNav);
