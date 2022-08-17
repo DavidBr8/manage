6f3c5439de214450051823d6367e4ef5eabb280a
David <brozdawid@gmail.com>
2022-08-17 09:45:01 +0200

    Mobile nav handling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 const navBtn = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.nav__mobile');
 const shadowHandle = document.querySelector('.pageshadow');
+const mobileNavItems = document.querySelectorAll('.nav__mobile-items-link');
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	mobileNav.classList.toggle('--active');
 	shadowHandle.classList.toggle('pageshadow-active');
 };
+mobileNavItems.forEach((item) =>
+	item.addEventListener('click', () => {
+		mobileNav.classList.toggle('--active');
+		shadowHandle.classList.toggle('pageshadow-active');
+		navBtn.classList.toggle('is-active');
+	})
+);
 navBtn.addEventListener('click', handleNav);
+console.log(mobileNavItems);
