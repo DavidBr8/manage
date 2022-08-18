932412ca716a0fb2646707f5410c1624ec2b69f6
David <brozdawid@gmail.com>
2022-08-18 18:35:53 +0200

    Some adjustments

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@ const navBtn = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.nav__mobile');
 const shadowHandle = document.querySelector('.pageshadow');
 const mobileNavItems = document.querySelectorAll('.nav__mobile-items-link');
+const navScrollHandle = document.querySelector('.nav');
+const footerYear = document.querySelector('.year');
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	mobileNav.classList.toggle('--active');
 	shadowHandle.classList.toggle('pageshadow-active');
 };
+
+const handleScroll = () => {
+	if (window.scrollY >= 50) {
+		navScrollHandle.classList.add('nav--scroll');
+	} else {
+		navScrollHandle.classList.remove('nav--scroll');
+	}
+};
+
 mobileNavItems.forEach((item) =>
 	item.addEventListener('click', () => {
 		mobileNav.classList.toggle('--active');
@@ -15,4 +26,10 @@ mobileNavItems.forEach((item) =>
 		navBtn.classList.toggle('is-active');
 	})
 );
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleScroll);
