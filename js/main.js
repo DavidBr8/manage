37a689da9a2ad5294e7b637f96283453842fdc4c
David <brozdawid@gmail.com>
2022-08-22 16:40:21 +0200

    Adjustments

--- js/main.js
+++ js/main.js
@@ -1,41 +1,45 @@
-const navBtn = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.nav__mobile');
-const shadowHandle = document.querySelector('.pageshadow');
-const mobileNavItems = document.querySelectorAll('.nav__mobile-items-link');
-const navScrollHandle = document.querySelector('.nav');
-const footerYear = document.querySelectorAll('.year');
-const navScrollPadding = document.querySelector('.nav__items');
+const handlePage = () => {
+	const navBtn = document.querySelector('.hamburger');
+	const mobileNav = document.querySelector('.nav__mobile');
+	const shadowHandle = document.querySelector('.pageshadow');
+	const mobileNavItems = document.querySelectorAll('.nav__mobile-items-link');
+	const navScrollHandle = document.querySelector('.nav');
+	const footerYear = document.querySelectorAll('.year');
+	const navScrollPadding = document.querySelector('.nav__items');
 
-const handleNav = () => {
-	navBtn.classList.toggle('is-active');
-	mobileNav.classList.toggle('--active');
-	shadowHandle.classList.toggle('pageshadow-active');
-};
-
-const handleScroll = () => {
-	if (window.scrollY >= 50) {
-		navScrollHandle.classList.add('nav--scroll');
-		navScrollPadding.classList.add('nav__items--scroll');
-	} else {
-		navScrollHandle.classList.remove('nav--scroll');
-		navScrollPadding.classList.remove('nav__items--scroll');
-	}
-};
-
-mobileNavItems.forEach((item) =>
-	item.addEventListener('click', () => {
+	const handleNav = () => {
+		navBtn.classList.toggle('is-active');
 		mobileNav.classList.toggle('--active');
 		shadowHandle.classList.toggle('pageshadow-active');
-		navBtn.classList.toggle('is-active');
-	})
-);
-const handleCurrentYear = () => {
-	const year = new Date().getFullYear();
-	footerYear.forEach((footerYear) => {
-		footerYear.innerText = year;
-	});
+	};
+
+	const handleScroll = () => {
+		if (window.scrollY >= 50) {
+			navScrollHandle.classList.add('nav--scroll');
+			navScrollPadding.classList.add('nav__items--scroll');
+		} else {
+			navScrollHandle.classList.remove('nav--scroll');
+			navScrollPadding.classList.remove('nav__items--scroll');
+		}
+	};
+
+	mobileNavItems.forEach((item) =>
+		item.addEventListener('click', () => {
+			mobileNav.classList.toggle('--active');
+			shadowHandle.classList.toggle('pageshadow-active');
+			navBtn.classList.toggle('is-active');
+		})
+	);
+	const handleCurrentYear = () => {
+		const year = new Date().getFullYear();
+		footerYear.forEach((footerYear) => {
+			footerYear.innerText = year;
+		});
+	};
+	handleCurrentYear();
+	navBtn.addEventListener('click', handleNav);
+	window.addEventListener('scroll', handleScroll);
+	AOS.init();
 };
-handleCurrentYear();
-navBtn.addEventListener('click', handleNav);
-window.addEventListener('scroll', handleScroll);
-AOS.init();
+
+handlePage();
